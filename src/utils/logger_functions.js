6ae489d796c4eae94ef8e5c999e542e11ce20906
wcosta <wellington.ferraz7@gmail.com>
2022-01-10 08:51:55 -0300

    logger changes

--- src/utils/logger_functions.js
+++ src/utils/logger_functions.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 
 const loggerFunction = (logMessage) => {
     if(logMessage.status === 'recognizing text')
         console.log(logMessage.progress.toPrecision(2));
+        return logMessage.progress.toPrecision(2);
 }
 
-export { loggerFunction } 
+const LoggerInUI = (logMessage) => {
+    let processing_text = document.querySelector('#processing-text');
+    if(logMessage.status === 'recognizing text') {
+        processing_text.textContent = 'Processando Cupom' + ' ' + logMessage.progress.toPrecision(2) + '%';
+    }
+
+    if(logMessage === 'Cupom Processado com Sucesso') {
+        processing_text.textContent = logMessage;
+    }
+
+    if(logMessage === 'Cupom Inválido') {
+        processing_text.textContent = logMessage;
+    }
+}
+
+export { loggerFunction, LoggerInUI } 
