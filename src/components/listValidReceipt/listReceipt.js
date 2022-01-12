da1be0c8f8ac280404c61ab95a654d2de803f397
wcosta <wellington.ferraz7@gmail.com>
2022-01-12 18:35:11 -0300

    splitting json received from BE to present it in the UI

--- src/components/listValidReceipt/listReceipt.js
+++ src/components/listValidReceipt/listReceipt.js
@@ -1,19 +1,48 @@
 //import './Receipt.css';
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import '../../pages/pages.css'
 
 const ReceiptList = (props) => {
-    return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
+    let consumidor = props.data.consumidor;
+    let estabelecimento = props.data.estabelecimento;
+    let produtos = props.data.produtos;
+    let ranking = props.data.ranking; //accuracy of the OCR process in Azure
+    let status = props.data.status_cupom;
+    console.log(props.data)
+    return (         
+        <div className='Card-Div-1'>
+            <div className='Card-Div-2'>
                 <Card.Header className='Card-Header-Local'>Valor Total: {props.data.valor_total}</Card.Header>
-                <Card.Text>Estabelecimento: {props.data.estabelecimento.nome}</Card.Text>
-                <Card.Text>End: {props.data.estabelecimento.endereco}</Card.Text>
-                <Card.Text>CNPJ: {props.data.estabelecimento.cnpj}</Card.Text>
-            </Card.Body>
+                <span className='Span-List-Card Padding-L-5'>ESTABELECIMENTO</span>
+                {Object.keys(estabelecimento).map(key => {
+                    return <Card.Text className='Padding-L-10'>{`${key.toUpperCase()}: ${estabelecimento[key]}`}</Card.Text>
+                })}
+                <div style={{'border-bottom': '1px solid #e6e6e6'}}></div>
+                <span className='Span-List-Card Padding-L-5'>CONSUMIDOR</span>
+                {Object.keys(consumidor).map(key => {
+                    return <Card.Text className='Padding-L-10'>{`${key.toUpperCase()}: ${consumidor[key]}`}</Card.Text>
+                })}
+                <div style={{'border-bottom': '1px solid #e6e6e6'}}></div>
+                <span className='Span-List-Card Padding-L-5'>PRODUTOS</span>
+                {produtos.map(produto => {
+                    return <Card.Text className='Padding-L-10'>{Object.keys(produto).map(key => {
+                        return ` ${key}: ${produto[key]}`;
+                    })}</Card.Text>
+                })}
+                <div style={{'border-bottom': '1px solid #e6e6e6'}}></div>
+                <span className='Span-List-Card Padding-L-5'>RANKING</span>
+                <Card.Text className='Padding-L-10'>Assertividade: {ranking}</Card.Text>
+                <div style={{'border-bottom': '1px solid #e6e6e6'}}></div>
+                <span className='Span-List-Card Padding-L-5'>STATUS</span>
+                <Card.Text className='Valid-Cupom'>{status}</Card.Text>
+            </div>
 
-        </Card>
+        </div>
     )
 }
 
 export default ReceiptList;
+
+
+//<Card.Text>{`${_key}: ${_value}`}</Card.Text>
