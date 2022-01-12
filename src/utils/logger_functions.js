4ae38a54634c409fb976c294bcece1f359bc88df
wcosta <wellington.ferraz7@gmail.com>
2022-01-12 18:35:41 -0300

    changes in the logging function

--- src/utils/logger_functions.js
+++ src/utils/logger_functions.js
@@ -9,7 +9,11 @@ const loggerFunction = (logMessage) => {
 const LoggerInUI = (logMessage) => {
     let processing_text = document.querySelector('#processing-text');
     if(logMessage.status === 'recognizing text') {
-        processing_text.textContent = 'Processando Cupom' + ' ' + logMessage.progress.toPrecision(2) + '%';
+        if(logMessage.progress.toPrecision(2) === '1.0') {
+            processing_text.textContent = 'Finalizando Processamento'
+        } else {
+            processing_text.textContent = 'Processando Cupom' + ' ' + logMessage.progress.toPrecision(2) + '%';
+        }
     }
 
     if(logMessage === 'Cupom Processado com Sucesso') {
@@ -17,6 +21,10 @@ const LoggerInUI = (logMessage) => {
     }
 
     if(logMessage === 'Cupom Inválido') {
+        processing_text.textContent = logMessage;
+    }
+
+    if(logMessage === 'CNPJ não encontrado no cupom fiscal') {
         processing_text.textContent = logMessage;
     }
 }
